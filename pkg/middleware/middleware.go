b5acd0bb937c3722420a69ceac7895132431e408
WahidinAji <a17wahidin@gmail.com>
2022-09-10 16:21:55 +0700

    fixed middleware to check if token is valid or user has token in the header-authorization.

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"github.com/golang-jwt/jwt/v4/request"
+
 	// "errors"
 	"presence-activities/pkg"
 
@@ -29,18 +31,82 @@ func jwtError(c *fiber.Ctx, err error) error {
 func IsAuth(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
-	_, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
 		return []byte(pkg.JWTSecret), nil
 	})
-
 	if err != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"status":  fiber.StatusUnauthorized,
-			"message": "Unauthorized",
+			"message": "Unauthenticated",
+			//"message": "Unauthorized",
 		})
 	}
 
+	//check if the token is nil
+	if token == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  fiber.StatusUnauthorized,
+			"message": "Unauthorized token nil",
+		})
+	}
+	//if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
+	//	fmt.Printf("%v %v", claims, claims.Issuer)
+	//} else {
+	//	fmt.Println(err)
+	//}
+
+	//check Authorization bearer token
+	bearer := c.Get("Authorization")
+	//check if bearer token is empty
+	if bearer == "" {
+		return c.Status(fiber.StatusNonAuthoritativeInformation).JSON(fiber.Map{
+			"status":  fiber.StatusNonAuthoritativeInformation,
+			"message": request.ErrNoTokenInRequest,
+		})
+	}
+	//check if bearer not equal with token.Raw that's already on the cookies server
+	if bearer != "Bearer "+token.Raw {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  fiber.StatusUnauthorized,
+			"message": "Unauthorized bearer",
+		})
+	}
+
+	//jwtToken := c.Get("token")
+	//fmt.Println(token.Claims.(*jwt.RegisteredClaims), token.Valid, token.Method, jwtToken, c.Get("Bearer"))
+	//fmt.Println(token.Raw)
+	//fmt.Println(cookie)
+	//fmt.Println(bearer)
+
+	//err = checkBearerToken(c, token.Raw)
+	//if err != nil {
+	//	return err
+	//}
+	//c.Get("Authorization","Bearer")
+
+	//#region
+	//token, err = jwt.Parse(cookie, func(t *jwt.Token) (interface{}, error) {
+	//	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	//		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	//	}
+	//	return []byte(pkg.JWTSecret), nil
+	//})
+	//if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+	//	fmt.Println(claims["email"], claims["exp"], token.Header["token"], token.Method)
+	//} else {
+	//	fmt.Println(err)
+	//}
+	//#endregion
+
+	//if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
+	//	fmt.Printf("%v %v", claims.Foo, claims.RegisteredClaims.Issuer)
+	//} else {
+	//	fmt.Println(err)
+	//}
+
+	//if all checked is accepted, user can access to their request
 	return c.Next()
+
 	// claims := token.Claims.(*jwt.RegisteredClaims)
 	// fmt.Println(claims.VerifyExpiresAt())
 	// token, err = jwt.Parse(cookie, func(t *jwt.Token) (interface{}, error) {
